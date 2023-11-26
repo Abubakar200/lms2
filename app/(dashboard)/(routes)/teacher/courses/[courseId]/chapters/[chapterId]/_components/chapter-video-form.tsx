e063ad2b48c701f58e7b2ab8146988e8a8e615dd
Abubakar200 <sheikhabubakr4@gmail.com>
2023-11-26 23:05:23 +0500

    changes in video form components

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -84,7 +84,7 @@ export const ChapterVideo = ({ initialData, courseId, chapterId }: ChapterVideoP
             }}
           />
           <div className="text-xs text-muted-foreground mt-4">
-            Upload this chapter's video
+            Upload this chapter&apos;s video
           </div>
         </div>
       )}
